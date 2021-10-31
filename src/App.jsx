5f5c53b3ad1fe8f94a638d0b9bd940ec0a37ea73
coffmanjrp <coffmanjrp@gmail.com>
2021-10-31 16:56:48 -0700

    Create global styled component and add all styles to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from './components/styles/Global';
+import Header from './components/Header';
+import { Container } from './components/styles/Container.styled';
+
+const theme = {
+  colors: {
+    header: '#ebfbff',
+    body: '#fff',
+    footer: '#003333',
+  },
+};
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Header />
+      <Container>
+        <h1>Hello World</h1>
+      </Container>
+    </ThemeProvider>
   );
 }
 
